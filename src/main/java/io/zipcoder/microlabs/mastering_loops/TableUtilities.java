0d0a9302525d72d348d3caebe6e0e5422f73912b
Chaz-Adams <caadams1922@gmail.com>
2024-03-05 18:42:46 -0500

    finished

--- src/main/java/io/zipcoder/microlabs/mastering_loops/TableUtilities.java
+++ src/main/java/io/zipcoder/microlabs/mastering_loops/TableUtilities.java
@@ -2,14 +2,28 @@ package io.zipcoder.microlabs.mastering_loops;
 
 public class TableUtilities {
     public static String getSmallMultiplicationTable() {
-        return null;
+        return getMultiplicationTable(5);
     }
 
     public static String getLargeMultiplicationTable() {
-        return null;
+        return getMultiplicationTable(10);
     }
 
     public static String getMultiplicationTable(int tableSize) {
-        return null;
+        String matrix = "";
+        for(int i = 1; i <= tableSize; i++){
+            for(int j = 1; j <= tableSize; j++){
+                if(i * j > 99){
+                    matrix += ""+i * j + " |";
+                }else if (i * j > 9) {
+                    matrix += " "+i * j + " |";
+                }else{
+                    matrix += "  "+i * j + " |";
+                }
+
+            }
+            matrix += "\n";
+        }
+        return matrix;
     }
 }
